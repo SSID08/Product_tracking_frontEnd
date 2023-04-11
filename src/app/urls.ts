e2b4df153f046a50fab488c4335578604739318c
SSID08 <112456108+SSID08@users.noreply.github.com>
2023-04-11 22:59:55 +0530

    Update urls.ts

--- src/app/urls.ts
+++ src/app/urls.ts
@@ -1,5 +1,5 @@
 export class URLS {
-    private static IP = "http://localhost:2000/api";
+    private static IP = "https://sorfmltest.loca.lt/api";
     //private static IP = "https://restapi.localho.st/";
     public static LIST = URLS.IP + "/list_assets";
     public static CREATE = URLS.IP + "/createproduct";
